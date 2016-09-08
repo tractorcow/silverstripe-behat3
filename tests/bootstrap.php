7e50e577665263bb0e6f15406d4db47f50315deb
Damian Mooyman <damian@silverstripe.com>
2016-09-08 16:31:48 +1200

    Fix include paths

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,4 +4,4 @@ $frameworkDir = basename($frameworkPath);
 if (!defined('BASE_PATH')) {
     define('BASE_PATH', dirname($frameworkPath));
 }
-require_once $frameworkPath . '/core/Core.php';
+require_once $frameworkPath . '/Core/Core.php';
