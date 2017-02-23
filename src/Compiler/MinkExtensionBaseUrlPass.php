17a76f9b57ce1f6498de647f2e77346b942dedb1
Damian Mooyman <damian@silverstripe.com>
2017-02-23 18:19:51 +1300

    Fix mink bootstrapping

--- src/Compiler/MinkExtensionBaseUrlPass.php
+++ src/Compiler/MinkExtensionBaseUrlPass.php
@@ -21,87 +21,45 @@ class MinkExtensionBaseUrlPass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
-        $frameworkPath = $container->getParameter('silverstripe_extension.framework_path');
-
-        global $_FILE_TO_URL_MAPPING;
-        if ($container->getParameter('behat.mink.base_url')) {
+        // Check if URL Is already valid
+        if ($baseURL = $container->getParameter('mink.base_url')) {
             // If base_url is already defined, also set it in the SilverStripe mapping
-            $_FILE_TO_URL_MAPPING[dirname($frameworkPath)] = $container->getParameter('behat.mink.base_url');
-        } elseif ($envPath = $this->findEnvironmentConfigFile($frameworkPath)) {
-            // Otherwise try to retrieve it from _ss_environment
-            include_once $envPath;
-            if (isset($_FILE_TO_URL_MAPPING)
-                && !($container->hasParameter('behat.mink.base_url') && $container->getParameter('behat.mink.base_url'))
-            ) {
-                $baseUrl = $this->findBaseUrlFromMapping(dirname($frameworkPath), $_FILE_TO_URL_MAPPING);
-                if ($baseUrl) {
-                    $container->setParameter('behat.mink.base_url', $baseUrl);
-                }
+            if (!getenv('SS_HOST')) {
+                putenv('SS_HOST=' . parse_url($baseURL, PHP_URL_HOST));
             }
+            return;
         }
 
-        if (!$container->getParameter('behat.mink.base_url')) {
+        // Find new url
+        if ($baseURL = $this->guessBaseURL()) {
+            $container->setParameter('mink.base_url', $baseURL);
+        } else {
             throw new \InvalidArgumentException(
                 '"base_url" not configured. Please specify it in your behat.yml configuration, ' .
-                'or in your _ss_environment.php configuration through $_FILE_TO_URL_MAPPING'
+                'or in your .env config with SS_HOST'
             );
         }
 
         // The Behat\MinkExtension\Extension class copies configuration into an internal hash,
         // we need to follow this pattern to propagate our changes.
-        $parameters = $container->getParameter('behat.mink.parameters');
-        $parameters['base_url'] = $container->getParameter('behat.mink.base_url');
-        $container->setParameter('behat.mink.parameters', $parameters);
+        $parameters = $container->getParameter('mink.parameters');
+        $parameters['base_url'] = $container->getParameter('mink.base_url');
+        $container->setParameter('mink.parameters', $parameters);
     }
 
     /**
-     * Try to auto-detect host for webroot based on _ss_environment.php data (unless explicitly set in behat.yml)
-     * Copied logic from Core.php, because it needs to be executed prior to {@link SilverStripeAwareInitializer}.
+     * Guess baseurl
      *
-     * @param string $path Absolute start path to search upwards from
-     * @return string Absolute path to environment file
+     * @return string
      */
-    protected function findEnvironmentConfigFile($path)
+    protected function guessBaseURL()
     {
-        $envPath = null;
-        $envFile = '_ss_environment.php'; //define the name of the environment file
-        $path = '.'; //define the dir to start scanning from (have to add the trailing slash)
-
-        //check this dir and every parent dir (until we hit the base of the drive)
-        do {
-            $path = realpath($path) . '/';
-            //if the file exists, then we include it, set relevant vars and break out
-            if (file_exists($path . $envFile)) {
-                $envPath = $path . $envFile;
-                break;
-            }
-        // here we need to check that the real path of the last dir and the next one are
-        // not the same, if they are, we have hit the root of the drive
-        } while (realpath($path) != realpath($path .= '../'));
-
-        return $envPath;
-    }
-
-    /**
-     * Copied logic from Core.php, because it needs to be executed prior to {@link SilverStripeAwareInitializer}.
-     *
-     * @param string $path Absolute start path to search upwards from
-     * @param array $mapping Map of paths to host names
-     * @return String URL
-     */
-    protected function findBaseUrlFromMapping($path, $mapping)
-    {
-        $fullPath = $path;
-        $url = null;
-        while ($path && $path != "/" && !preg_match('/^[A-Z]:\\\\$/', $path)) {
-            if (isset($mapping[$path])) {
-                $url = $mapping[$path] . str_replace(DIRECTORY_SEPARATOR, '/', substr($fullPath, strlen($path)));
-                break;
-            } else {
-                $path = dirname($path); // traverse up
-            }
+        if (isset($_REQUEST['HTTP_HOST'])) {
+            return 'http://'.$_REQUEST['HTTP_HOST'].'/';
         }
-
-        return $url;
+        if (getenv('SS_HOST')) {
+            return 'http://'.getenv('SS_HOST').'/';
+        }
+        return null;
     }
 }
